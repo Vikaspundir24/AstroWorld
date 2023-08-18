60fb824c347fcebd524eb9c208296d2fc5c44ee3
vikas pundir <vikaspundiruser@gmail.com>
2023-08-18 18:17:14 +0530

    LAST API CRASHED, NEW API ADDED TO TEST

--- src/components/DataCard.js
+++ src/components/DataCard.js
@@ -1,30 +1,23 @@
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import "./DataCard.css";
 
-import React , {useEffect , useState} from 'react'
-import { useSelector } from 'react-redux'
-import './DataCard.css'
-
-
-
-import Backdrop from '@mui/material/Backdrop';
-import CircularProgress from '@mui/material/CircularProgress';
-
-
-
-
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
 
 function DataCard() {
+  const Sign = useSelector((state) => state.inputData.Sign);
+  const Day = useSelector((state) => state.inputData.Day);
+  const [data, setData] = useState("");
 
-const Sign = useSelector(state => state.inputData.Sign)
-const Day = useSelector(state => state.inputData.Day)
-const [data ,  setData] = useState("")
+  useEffect(() => {
+    const axios = require("axios");
+    const options = {
+      url: `https://us-central1-tf-natal.cloudfunctions.net/horoscopeapi_test?token=mmEUtLATc8w_UNnHuR2&sign=${Sign}&date=${Day}`,
+      method: "GET",
+    };
 
-
-
-
-useEffect(() => {
-       const axios = require("axios");
-
-       const options = {
+    /*    const options = {
                            method: 'POST',
                            url: 'https://sameer-kumar-aztro-v1.p.rapidapi.com/',
                            params: {sign:Sign , day: Day},
@@ -32,55 +25,60 @@ useEffect(() => {
                                       'X-RapidAPI-Key': '00b9e8b4e8msha01832e151a40fbp19aeddjsn714563214832',
                                       'X-RapidAPI-Host': 'sameer-kumar-aztro-v1.p.rapidapi.com'
                                     }
-                        };
+                        }; */
 
-        axios.request(options).then(function (response) {
-          setData(response.data);
-          console.error(response.data);
-        
-         
-     
-        }).catch(function (error) {
+    axios
+      .request(options)
+      .then(function (response) {
+        setData(response.data);
+        console.error(response.data);
+      })
+      .catch(function (error) {
         console.error(error);
-        });
-    },[Sign , Day])
+      });
+  }, [Sign, Day]);
 
-    return (
-      <>
-      { !data ? <Backdrop
-        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-        open
-      >
-        <CircularProgress color="inherit" />
-      </Backdrop>
-      : <div className="container wrapper">
-        <h1>{data.current_date}</h1>
-        <h1>{Sign}</h1>
-        <div className="sides">
-        <div className="left-side">
-          <h2>Color : <span>{data.color}</span></h2>
-          <h2>Compatibilty : <span>{data.compatibility}</span></h2>
-          <h2>Lucky Number : <span>{data.lucky_number}</span></h2>
-          <h2>lucky Time : <span>{data.lucky_time}</span> </h2>
-          <h2>Mood : <span>{data.mood}</span></h2>
+  return (
+    <>
+      {!data ? (
+        <Backdrop
+          sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+          open
+        >
+          <CircularProgress color="inherit" />
+        </Backdrop>
+      ) : (
+        <div className="container wrapper">
+          <h1>{data.current_date}</h1>
+          <h1>{Sign}</h1>
+          <div className="sides">
+            <div className="left-side">
+              <h2>
+                Color : <span>{data.color}</span>
+              </h2>
+              <h2>
+                Compatibilty : <span>{data.compatibility}</span>
+              </h2>
+              <h2>
+                Lucky Number : <span>{data.lucky_number}</span>
+              </h2>
+              <h2>
+                lucky Time : <span>{data.lucky_time}</span>{" "}
+              </h2>
+              <h2>
+                Mood : <span>{data.mood}</span>
+              </h2>
+            </div>
+            <div className="right-side">
+              <h2 className="desc">
+                Description : <span>{data.description}</span>
+              </h2>
+            </div>
+          </div>
         </div>
-        <div className="right-side">
-          <h2 className='desc'>Description : <span>{data.description}</span>
-          </h2>
-       
-        </div>
-        </div>
-     
-     
-      </div>}
-
-      
-      
-      </>
-      )
-
-
-
+      )}
+    </>
+  );
 }
 
-export default DataCard
+export default DataCard;
